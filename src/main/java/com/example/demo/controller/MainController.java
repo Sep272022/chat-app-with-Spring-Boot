600d3f4da3039ebd58eea14942ae922eb80c890f
Joonwoo Lee <jlee228@ncsu.edu>
2023-05-12 21:43:00 -0400

    Seperated AuthController from MainContoller

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -16,48 +16,6 @@ import com.example.demo.service.UserService;
 @Controller
 public class MainController {
 
-  @Autowired
-  private UserService userService;
-  
-  @GetMapping("/register")
-  public String showForm(Model model) {
-    User user = new User();
-    model.addAttribute("user", user);
-
-    List<String> listProfession = Arrays.asList("Developer", "Tester", "Architect");
-    model.addAttribute("listProfession", listProfession);
-
-    return "register_form";
-  }
-
-  @PostMapping("/register")
-  public String submitForm(@ModelAttribute("user") User user) {
-    System.out.println(user);
-    userService.createUser(user);
-    return "register_success";
-  }
-
-  @GetMapping("/login")
-  public String showLoginPage() {
-    return "login";
-  }
-
-  @PostMapping("/login")
-  public String login(Model model) {
-    return "index";
-  }
-
-  @PostMapping("/login/process")
-  public String performLogin() {
-    System.out.println("performLogin");
-    return "index";
-  }
-
-  @PostMapping("/logout")
-  public String logout(Model model) {
-    return "login";
-  }
-
   @GetMapping("/index") 
   public String showIndexPage() {
     return "index";
