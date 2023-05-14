3bbee612dd9c62ef3ca2e8eabab44020fdb571dc
Joonwoo Lee <jlee228@ncsu.edu>
2023-05-13 22:41:24 -0400

    working on login functionality

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,71 +1,61 @@
 package com.example.demo.config;
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
 
 @Configuration
 @EnableWebSecurity
-@EnableWebMvc
 public class SecurityConfig {
 
   String[] staticResources = {
+    "/resources/**",
+    "/static/**",
     "/css/**",
-    "/images/**",
-    "/fonts/**",
-    "/scripts/**",};
+    "/js/**",
+    "/*.js"};
 
-  @Bean
-  InMemoryUserDetailsManager userDetailsManager() {
-    UserDetails user1 = User.withUsername("user1")
-                            .password(passwordEncoder().encode("111"))
-                            .roles("USER")
-                            .build();
+  // @Bean
+  // InMemoryUserDetailsManager userDetailsManager() {
+  //   UserDetails user1 = User.withUsername("user1")
+  //                           .password(passwordEncoder().encode("111"))
+  //                           .roles("USER")
+  //                           .build();
                             
-    UserDetails user2 = User.withUsername("user2")
-                            .password(passwordEncoder().encode("111"))
-                            .roles("USER")
-                            .build();
+  //   UserDetails user2 = User.withUsername("user2")
+  //                           .password(passwordEncoder().encode("111"))
+  //                           .roles("USER")
+  //                           .build();
             
-    UserDetails admin = User.withUsername("user3")
-                            .password(passwordEncoder().encode("111"))
-                            .roles("ADMIN")
-                            .build();
+  //   UserDetails admin = User.withUsername("user3")
+  //                           .password(passwordEncoder().encode("111"))
+  //                           .roles("ADMIN")
+  //                           .build();
 
-    return new InMemoryUserDetailsManager(user1, user2, admin);
-  }
+  //   return new InMemoryUserDetailsManager(user1, user2, admin);
+  // }
 
 
   // https://www.baeldung.com/spring-security-login 
   // https://docs.spring.io/spring-security/reference/5.8/migration/servlet/config.html
   @Bean
   SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-      http.authorizeHttpRequests((authz) -> authz.requestMatchers("/css/**", "/js/**").permitAll().requestMatchers("/admin/**")
-              .hasRole("ADMIN")
-              .requestMatchers("/user/**")
-              .hasAnyRole("USER", "ADMIN")
-              .requestMatchers(staticResources)
-              .permitAll()
-              .anyRequest()
-              .authenticated())
+      http.authorizeHttpRequests((authz) -> authz
+              .requestMatchers(staticResources).permitAll()
+              .requestMatchers("/login", "/register").permitAll()
+              .requestMatchers("/admin/**").hasRole("ADMIN")
+              .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
+              .anyRequest().authenticated())
               .formLogin(login -> login.loginPage("/login")
-                      .loginProcessingUrl("/login/process")
-                      .defaultSuccessUrl("/index", true)
-                      .failureUrl("/login?error=true")
+                      .successForwardUrl("/index")
+                      .failureUrl("/login?error")
                       .permitAll())
               .logout(logout -> logout.logoutUrl("/logout")
-                      .logoutSuccessUrl("/login?logout=true")
+                      .logoutSuccessUrl("/login?logout")
                       .invalidateHttpSession(true)
                       .deleteCookies("JSESSIONID")
                       .permitAll())
@@ -76,7 +66,7 @@ public class SecurityConfig {
 
   // @Bean
   // WebSecurityCustomizer webSecurityCustomizer() {
-  //     return (web) -> web.ignoring().requestMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
+  //     return (web) -> web.ignoring().requestMatchers(staticResources);
   // }
 
   @Bean 
