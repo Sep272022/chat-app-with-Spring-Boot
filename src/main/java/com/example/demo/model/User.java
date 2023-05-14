1142eed7056d439e7e0434c9eb52cc994a38e8fa
Joonwoo Lee <jlee228@ncsu.edu>
2023-05-13 23:15:01 -0400

    fixed an issue of not working with MongoDB

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -9,7 +9,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class User {
 
   @Id
-  private Long id;
+  private String id;
   private String name;
   private String email;
   private String password;
