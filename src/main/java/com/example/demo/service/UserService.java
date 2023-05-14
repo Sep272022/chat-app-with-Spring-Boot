34fedd14cd808182b4e543db1c91b0c3f1534384
Joonwoo Lee <jlee228@ncsu.edu>
2023-05-13 23:57:48 -0400

    logic to encrypt the password for user creation and verification

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.model.User;
@@ -14,7 +15,11 @@ public class UserService {
   @Autowired
   private MongoTemplate mongoTemplate;
 
+  @Autowired
+  private PasswordEncoder passwordEncoder;
+
   public User createUser(User user) {
+    user.setPassword(passwordEncoder.encode(user.getPassword()));
     return mongoTemplate.save(user);
   }
 
@@ -22,6 +27,14 @@ public class UserService {
     Query query = new Query();
     query.addCriteria(Criteria.where("email").is(email));
     return mongoTemplate.findOne(query, User.class);
+  }
+
+  public boolean verifyUser(User user) {
+    User found = findUserByEmail(user.getEmail());
+    if (found != null) {
+      return passwordEncoder.matches(user.getPassword(), found.getPassword());
+    }
+    return false;
   }
 
 }
