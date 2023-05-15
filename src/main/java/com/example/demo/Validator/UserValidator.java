7411e49da50b0b5f02ada0ebf7d92b1e94f42f04
Joonwoo Lee <jlee228@ncsu.edu>
2023-05-14 22:16:24 -0400

    added logic to validate user in signup using validator

---
+++ src/main/java/com/example/demo/Validator/UserValidator.java
@@ -0,0 +1,39 @@
+package com.example.demo.Validator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+
+@Component
+public class UserValidator implements Validator {
+
+  @Autowired
+  private UserService userService;
+
+  @Override
+  public boolean supports(Class<?> clazz) {
+    return User.class.isAssignableFrom(clazz);
+  }
+
+  @Override
+  public void validate(Object target, Errors errors) {
+    User user = (User) target;
+
+    ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "NotEmpty.user.email");
+    ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "NotEmpty.user.password");
+    // ValidationUtils.rejectIfEmptyOrWhitespace(errors, "confirmPassword", "NotEmpty.user.confirmPassword");
+    ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "NotEmpty.user.name");
+    ValidationUtils.rejectIfEmptyOrWhitespace(errors, "gender", "NotEmpty.user.gender");
+    ValidationUtils.rejectIfEmptyOrWhitespace(errors, "profession", "NotEmpty.user.profession");
+    // TODO: validation for birthday
+
+
+
+  }
+  
+}
