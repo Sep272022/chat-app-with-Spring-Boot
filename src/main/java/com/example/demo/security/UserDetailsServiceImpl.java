9a417fcbc3422355995da592855914fcce27d967
Joonwoo Lee <jlee228@ncsu.edu>
2023-05-17 00:01:14 -0400

    implemented to work with Spring Security

---
+++ src/main/java/com/example/demo/security/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.example.demo.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+  @Autowired
+  private UserService userService;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    User user = userService.findUserByEmail(username);
+    System.out.println("loadUserByUsername: " + user);
+    if (user == null) {
+      throw new UsernameNotFoundException("User not found");
+    }
+    
+    return new UserDetailsImpl(user);
+  }
+  
+}
