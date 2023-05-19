fdc6d2ab3839f6d50f64ae3844423e36c0b5bf7e
Joonwoo Lee <jlee228@ncsu.edu>
2023-05-18 21:57:04 -0400

    working on a chat system

---
+++ src/main/java/com/example/demo/controller/ChatController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+import com.example.demo.model.ChatMessage;
+
+@Controller
+public class ChatController {
+  
+  @MessageMapping("/chat")
+  @SendTo("/topic/messages")
+  public ChatMessage getMessage(ChatMessage message) {
+    return message;
+  }
+}
