8e8725445b325efb4d7f2c5c22f0f1cb26cc11c3
Joonwoo Lee <jlee228@ncsu.edu>
2023-05-18 22:40:36 -0400

    recipient and date added to ChatMessage

--- src/main/java/com/example/demo/model/ChatMessage.java
+++ src/main/java/com/example/demo/model/ChatMessage.java
@@ -1,9 +1,14 @@
 package com.example.demo.model;
 
+import java.sql.Date;
+
 public class ChatMessage {
   private String from;
   private String text;
+  private String to;
+  private Date date;
   
+
   public String getFrom() {
     return from;
   }
@@ -16,6 +21,17 @@ public class ChatMessage {
   public void setText(String text) {
     this.text = text;
   }
-
+  public String getTo() {
+    return to;
+  }
+  public void setTo(String to) {
+    this.to = to;
+  }
+  public Date getDate() {
+    return date;
+  }
+  public void setDate(Date date) {
+    this.date = date;
+  }
   
 }
