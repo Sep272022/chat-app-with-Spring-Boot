856414f2630db695c89db69ed2d128db4c00541b
Joonwoo Lee <jlee228@ncsu.edu>
2023-05-23 21:26:16 -0400

    Rename ChatController to ChatRestContoller

--- src/main/java/com/example/demo/controller/ChatRestController.java
+++ src/main/java/com/example/demo/controller/ChatRestController.java
@@ -1,16 +1,20 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.ChatRoomDTO;
 import com.example.demo.service.ChatRoomService;
 
 @RestController
-public class ChatController {
+public class ChatRestController {
 
   @Autowired
   private ChatRoomService chatRoomService;
@@ -20,4 +24,13 @@ public class ChatController {
     return ResponseEntity.ok(chatRoomService.createChatRoom(chatRoom));
   }
 
+  @GetMapping("/chatrooms/{id}")
+  public ResponseEntity<ChatRoomDTO> getChatRoomById(String id) {
+    return ResponseEntity.ok(chatRoomService.findChatRoomById(id));
+  }
+
+  @GetMapping("/chatrooms")
+  public ResponseEntity<List<ChatRoomDTO>> getAllChatRooms(@RequestParam(value = "userId", required = true) String userId) {
+    return ResponseEntity.ok(chatRoomService.findChatRoomsByUserId(userId));
+  }
 }
