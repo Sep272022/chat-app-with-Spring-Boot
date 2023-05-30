4681de571adcf3a6510be1603fe71b460c0b6465
Joonwoo Lee <jlee228@ncsu.edu>
2023-05-30 14:06:04 -0400

    Remove unnecessary attributes

--- src/main/java/com/example/demo/model/UserDTO.java
+++ src/main/java/com/example/demo/model/UserDTO.java
@@ -1,81 +1,51 @@
 package com.example.demo.model;
 
-import java.util.Date;
 import java.util.Set;
 
-
 public class UserDTO {
+
   private String id;
   private String name;
   private String email;
-  private String gender;
-  private String note;
-  private boolean married; 
-  private Date birthday;
-  private String profession;
   private Set<Role> roles;
-
   private Set<String> chatRoomIds;
-
-  
 
   public String getId() {
     return id;
   }
+
   public void setId(String id) {
     this.id = id;
   }
+
   public String getName() {
     return name;
   }
+
   public void setName(String name) {
     this.name = name;
   }
+
   public String getEmail() {
     return email;
   }
+
   public void setEmail(String email) {
     this.email = email;
   }
-  public String getGender() {
-    return gender;
-  }
-  public void setGender(String gender) {
-    this.gender = gender;
-  }
-  public String getNote() {
-    return note;
-  }
-  public void setNote(String note) {
-    this.note = note;
-  }
-  public boolean isMarried() {
-    return married;
-  }
-  public void setMarried(boolean married) {
-    this.married = married;
-  }
-  public Date getBirthday() {
-    return birthday;
-  }
-  public void setBirthday(Date birthday) {
-    this.birthday = birthday;
-  }
-  public String getProfession() {
-    return profession;
-  }
-  public void setProfession(String profession) {
-    this.profession = profession;
-  }
+
   public Set<Role> getRoles() {
     return roles;
   }
+
   public void setRoles(Set<Role> roles) {
     this.roles = roles;
   }
+
   public Set<String> getChatRoomIds() {
     return chatRoomIds;
   }
+
   public void setChatRoomIds(Set<String> chatRoomIds) {
     this.chatRoomIds = chatRoomIds;
   }
