e2af0e04773dc329b6237cea9962a6e471ad9ab4
Joonwoo Lee <jlee228@ncsu.edu>
2023-06-04 19:16:31 -0400

    Change PostMapping to DeleteMapping on leaveChatRoom()

--- src/main/java/com/example/demo/controller/ChatRestController.java
+++ src/main/java/com/example/demo/controller/ChatRestController.java
@@ -1,18 +1,17 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.ChatRoomDTO;
+import com.example.demo.service.ChatRoomService;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.demo.model.ChatRoomDTO;
-import com.example.demo.service.ChatRoomService;
 
 @RestController
 public class ChatRestController {
@@ -21,7 +20,9 @@ public class ChatRestController {
   private ChatRoomService chatRoomService;
 
   @PostMapping("/chatrooms")
-  public ResponseEntity<ChatRoomDTO> createChatRoom(@RequestBody ChatRoomDTO chatRoom) {
+  public ResponseEntity<ChatRoomDTO> createChatRoom(
+    @RequestBody ChatRoomDTO chatRoom
+  ) {
     return ResponseEntity.ok(chatRoomService.createChatRoom(chatRoom));
   }
 
@@ -31,12 +32,17 @@ public class ChatRestController {
   }
 
   @GetMapping("/chatrooms")
-  public ResponseEntity<List<ChatRoomDTO>> getAllChatRooms(@RequestParam(value = "userId", required = true) String userId) {
+  public ResponseEntity<List<ChatRoomDTO>> getAllChatRooms(
+    @RequestParam(value = "userId", required = true) String userId
+  ) {
     return ResponseEntity.ok(chatRoomService.findChatRoomsByUserId(userId));
   }
 
-  @PostMapping("chatrooms/{userId}/leave/{chatRoomId}")
-  public ResponseEntity<ChatRoomDTO> leaveChatRoom(@PathVariable String userId, @PathVariable String chatRoomId) {
+  @DeleteMapping("chatrooms/{userId}/leave/{chatRoomId}")
+  public ResponseEntity<ChatRoomDTO> leaveChatRoom(
+    @PathVariable(value = "userId", required = true) String userId,
+    @PathVariable(value = "chatRoomId", required = true) String chatRoomId
+  ) {
     System.out.println("userId: " + userId + ", chatRoomId: " + chatRoomId);
     return ResponseEntity.ok(chatRoomService.leaveChatRoom(userId, chatRoomId));
   }
