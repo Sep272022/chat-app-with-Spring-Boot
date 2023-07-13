df8bd8a74881ed23176d58722cdb8534dad3d187
Joonwoo Lee <jlee228@ncsu.edu>
2023-07-13 17:49:42 -0400

    Remove unnecessary code

--- src/main/java/com/example/demo/service/ChatMessageService.java
+++ src/main/java/com/example/demo/service/ChatMessageService.java
@@ -1,7 +1,8 @@
 package com.example.demo.service;
 
+import com.example.demo.model.ChatMessage;
+import com.example.demo.model.ChatMessageDTO;
 import java.util.List;
-
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -9,12 +10,9 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
-import com.example.demo.model.ChatMessage;
-import com.example.demo.model.ChatMessageDTO;
-
 @Service
 public class ChatMessageService {
-  
+
   @Autowired
   private MongoTemplate mongoTemplate;
 
@@ -26,29 +24,6 @@ public class ChatMessageService {
 
   public void saveChatMessage(ChatMessage chatMessage) {
     mongoTemplate.save(chatMessage);
-  }
-
-  public List<ChatMessage> findByFromAndTo(String fromUserId, String toUserId) {
-    Query query = new Query();
-    Criteria criteria = new Criteria();
-    criteria.orOperator(
-      Criteria.where("fromUserId").is(fromUserId).and("toUserId").is(toUserId),
-      Criteria.where("fromUserId").is(toUserId).and("toUserId").is(fromUserId)
-    );
-    query.addCriteria(criteria);
-    return mongoTemplate.find(query, ChatMessage.class);
-  }
-  
-  public List<ChatMessage> findByFrom(String fromUserId) {
-    Query query = new Query();
-    query.addCriteria(Criteria.where("fromUserId").is(fromUserId));
-    return mongoTemplate.find(query, ChatMessage.class);
-  }
-
-  public List<ChatMessage> findByTo(String toUserId) {
-    Query query = new Query();
-    query.addCriteria(Criteria.where("toUserId").is(toUserId));
-    return mongoTemplate.find(query, ChatMessage.class);
   }
 
   public List<ChatMessage> findAllByUserId(String userId) {
@@ -67,10 +42,16 @@ public class ChatMessageService {
   }
 
   public ChatMessageDTO convertChatMessageToDTO(ChatMessage chatMessage) {
-    ChatMessageDTO chatMessageDTO = modelMapper.map(chatMessage, ChatMessageDTO.class);
-    chatMessageDTO.setFromUser(userService.findUserById(chatMessage.getFromUserId()));
-    chatMessageDTO.setToUser(userService.findUserById(chatMessage.getToUserId()));
+    ChatMessageDTO chatMessageDTO = modelMapper.map(
+      chatMessage,
+      ChatMessageDTO.class
+    );
+    chatMessageDTO.setFromUser(
+      userService.findUserById(chatMessage.getFromUserId())
+    );
+    chatMessageDTO.setToUser(
+      userService.findUserById(chatMessage.getToUserId())
+    );
     return chatMessageDTO;
   }
-
 }
