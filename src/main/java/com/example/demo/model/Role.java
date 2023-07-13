c3ef74d4991f5bc07674e6a633b019eef87b6afe
Joonwoo Lee <jlee228@ncsu.edu>
2023-07-13 18:56:39 -0400

    Role added

--- src/main/java/com/example/demo/model/Role.java
+++ src/main/java/com/example/demo/model/Role.java
@@ -6,25 +6,34 @@ import org.springframework.security.core.GrantedAuthority;
 
 @Document("role")
 public class Role implements GrantedAuthority {
+
   @Id
   private String id;
+
   private String name;
+
+  public Role(String name) {
+    this.name = name;
+  }
+
   public String getId() {
     return id;
   }
+
   public void setId(String id) {
     this.id = id;
   }
+
   public String getName() {
     return name;
   }
+
   public void setName(String name) {
     this.name = name;
   }
+
   @Override
   public String getAuthority() {
     return name;
   }
-
-  
 }
--- src/main/java/com/example/demo/repository/RoleRepository.java
+++ src/main/java/com/example/demo/repository/RoleRepository.java
@@ -1,5 +1,11 @@
 package com.example.demo.repository;
 
-public class RoleRepository {
-  
+import com.example.demo.model.Role;
+import java.util.Optional;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+public interface RoleRepository extends MongoRepository<Role, String> {
+  @Query("{name : ?0}")
+  Optional<Role> findRoleByName(String name);
 }
